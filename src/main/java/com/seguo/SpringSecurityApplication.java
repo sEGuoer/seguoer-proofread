61260953f2337c53201441088518cfa714f297fa
sEGuoer <709074535@qq.com>
2023-11-20 15:53:23 +0800

    change database

--- src/main/java/com/seguo/SpringSecurityApplication.java
+++ src/main/java/com/seguo/SpringSecurityApplication.java
@@ -1,18 +1,7 @@
 package com.seguo;
 
-import com.seguo.entity.Permission;
-import com.seguo.entity.Role;
-import com.seguo.entity.User;
-import com.seguo.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
