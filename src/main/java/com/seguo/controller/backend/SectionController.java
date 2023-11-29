e2621286e07e512871057100b85d14e222d18964
sEGuoer <709074535@qq.com>
2023-11-29 17:18:01 +0800

    update section

--- src/main/java/com/seguo/controller/backend/SectionController.java
+++ src/main/java/com/seguo/controller/backend/SectionController.java
@@ -2,25 +2,30 @@ package com.seguo.controller.backend;
 
 import com.seguo.dto.SectionDto;
 import com.seguo.entity.Section;
+import com.seguo.service.ProofreadService;
 import com.seguo.service.SectionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Controller("backendSectionController")
 @RequestMapping("/admin/sections/")
 public class SectionController {
     @Autowired
     SectionService sectionService;
-
+    @Autowired
+    ProofreadService proofreadService;
     @GetMapping("create")
-    String create(Model model) {
+    String create(@RequestParam("collection_id") Long collectionId, Model model) {
+        model.addAttribute("collection", proofreadService.findById(collectionId).get());
         model.addAttribute("section", new Section());
         return "backend/section/create";
     }
@@ -32,6 +37,41 @@ public class SectionController {
             return "backend/section/create";
         }
         sectionService.save(sectionDto);
+        return "redirect:/admin/collections/edit/" + sectionDto.getCollection_id();
+    }
+
+    @GetMapping("edit/{id}")
+    String edit(
+            @PathVariable Long id,
+            @RequestParam("collection_id") Long collectionId,
+            Model model
+    ) {
+        Optional<Section> optionalSection = sectionService.findById(id);
+        if (optionalSection.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            Section section = optionalSection.get();
+            model.addAttribute("collection", proofreadService.findById(collectionId).get());
+            model.addAttribute("section", section);
+            return "backend/section/edit";
+        }
+    }
+
+    @PutMapping("update")
+    String update(
+            @Validated @ModelAttribute("section") SectionDto sectionDto,
+            BindingResult result,
+            Model model
+    ) {
+        if (result.hasErrors()) {
+            model.addAttribute("collection", proofreadService.findById(sectionDto.getCollection_id()).get());
+            model.addAttribute("section", sectionDto);
+
+            return "backend/section/edit";
+        }
+
+        sectionService.save(sectionDto);
+
         return "redirect:/admin/collections/edit/" + sectionDto.getCollection_id();
     }
 }
